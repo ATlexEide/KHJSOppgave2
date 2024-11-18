a74bcd5c1bf3c76c6c010e598c60369f607f81eb
Alexander Eide <atlexeide@gmail.com>
2024-11-18 20:18:48 +0100

    Add solution to 12th challenge

--- main.js
+++ main.js
@@ -108,6 +108,6 @@ fizzbuzz(50);
 //example: factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
 function factorial(n) {
   if (n === 0) return 1;
-  return; /// din kode her
+  return n * factorial(n - 1);
 }
-console.log(factorial(5));
+console.log(factorial(7));
